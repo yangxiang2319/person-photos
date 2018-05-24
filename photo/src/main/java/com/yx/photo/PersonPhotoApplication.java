6461872a87670fa70fbc7188c213aa73b6330bea
yangxiang2319 <1010716322@qq.com>
2018-05-24 19:13:03 +0800

    添加相册页面模块

---
+++ photo/src/main/java/com/yx/photo/PersonPhotoApplication.java
@@ -0,0 +1,12 @@
+package com.yx.photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonPhotoApplication.class, args);
+	}
+}
